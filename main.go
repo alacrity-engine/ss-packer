8c31bf3fe7eef32353d6f50a47e554d6329d4eaa
zergon321 <maximgradan@gmail.com>
2023-10-08 12:38:16 +0300

    Now affects the whole project.

--- main.go
+++ main.go
@@ -4,19 +4,22 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path"
+	"strings"
 
 	codec "github.com/alacrity-engine/resource-codec"
+	"github.com/golang-collections/collections/queue"
 	bolt "go.etcd.io/bbolt"
 )
 
 var (
-	spritesheetsMetaPath string
-	resourceFilePath     string
+	projectPath      string
+	resourceFilePath string
 )
 
 func parseFlags() {
-	flag.StringVar(&spritesheetsMetaPath, "spritesheets-meta",
-		"./spritesheets.yml", "Path to the spritesheets metadata file.")
+	flag.StringVar(&projectPath, "project", ".",
+		"Path to the project to pack spritesheets for.")
 	flag.StringVar(&resourceFilePath, "out", "./stage.res",
 		"Resource file to store animations and spritesheets.")
 
@@ -26,53 +29,91 @@ func parseFlags() {
 func main() {
 	parseFlags()
 
-	data, err := os.ReadFile(spritesheetsMetaPath)
-	handleError(err)
-	spritesheetMetas, err := ReadSpritesheetsData(data)
-	handleError(err)
-	spritesheetDatas := make([]codec.SpritesheetData,
-		0, len(spritesheetMetas))
-
-	for i := 0; i < len(spritesheetMetas); i++ {
-		spritesheetMeta := spritesheetMetas[i]
-		spritesheetDatas = append(spritesheetDatas,
-			spritesheetMeta.ToSpritesheetData())
-	}
-
 	resourceFile, err := bolt.Open(resourceFilePath, 0666, nil)
 	handleError(err)
 	defer resourceFile.Close()
 
-	for i := 0; i < len(spritesheetDatas); i++ {
-		spritesheetMeta := spritesheetMetas[i]
-		spritesheetData := spritesheetDatas[i]
+	entries, err := os.ReadDir(projectPath)
+	handleError(err)
 
-		err = resourceFile.Update(func(tx *bolt.Tx) error {
-			buck, err := tx.CreateBucketIfNotExists([]byte("spritesheets"))
+	traverseQueue := queue.New()
 
-			if err != nil {
-				return err
-			}
+	if len(entries) <= 0 {
+		return
+	}
 
-			if buck == nil {
-				return fmt.Errorf("no spritesheets bucket present")
-			}
-
-			textureBytes, err := spritesheetData.ToBytes()
-
-			if err != nil {
-				return err
-			}
-
-			err = buck.Put([]byte(spritesheetMeta.Name), textureBytes)
-
-			if err != nil {
-				return err
-			}
-
-			return nil
+	for _, entry := range entries {
+		traverseQueue.Enqueue(FileTracker{
+			EntryPath: ".",
+			Entry:     entry,
 		})
+	}
+
+	for traverseQueue.Len() > 0 {
+		fsEntry := traverseQueue.Dequeue().(FileTracker)
+
+		if fsEntry.Entry.IsDir() {
+			entries, err = os.ReadDir(path.Join(fsEntry.EntryPath, fsEntry.Entry.Name()))
+			handleError(err)
+
+			for _, entry := range entries {
+				traverseQueue.Enqueue(FileTracker{
+					EntryPath: path.Join(fsEntry.EntryPath, fsEntry.Entry.Name()),
+					Entry:     entry,
+				})
+			}
+
+			continue
+		}
+
+		if !strings.HasSuffix(fsEntry.Entry.Name(), ".ss.yml") {
+			continue
+		}
+
+		data, err := os.ReadFile(path.Join(fsEntry.EntryPath, fsEntry.Entry.Name()))
 		handleError(err)
+		spritesheetMetas, err := ReadSpritesheetsData(data)
+		handleError(err)
+		spritesheetDatas := make([]codec.SpritesheetData,
+			0, len(spritesheetMetas))
+
+		for i := 0; i < len(spritesheetMetas); i++ {
+			spritesheetMeta := spritesheetMetas[i]
+			spritesheetDatas = append(spritesheetDatas,
+				spritesheetMeta.ToSpritesheetData())
+		}
+
+		for i := 0; i < len(spritesheetDatas); i++ {
+			spritesheetMeta := spritesheetMetas[i]
+			spritesheetData := spritesheetDatas[i]
+
+			err = resourceFile.Update(func(tx *bolt.Tx) error {
+				buck, err := tx.CreateBucketIfNotExists([]byte("spritesheets"))
+
+				if err != nil {
+					return err
+				}
+
+				if buck == nil {
+					return fmt.Errorf("no spritesheets bucket present")
+				}
+
+				textureBytes, err := spritesheetData.ToBytes()
+
+				if err != nil {
+					return err
+				}
+
+				err = buck.Put([]byte(spritesheetMeta.Name), textureBytes)
+
+				if err != nil {
+					return err
+				}
+
+				return nil
+			})
+			handleError(err)
+		}
 	}
 }
 
