0f49e92c6a0685cda22b245b8fffcdd8f456a6c1
zergon321 <maximgradan@gmail.com>
2023-10-20 12:44:51 +0300

    Fields added.

--- meta.go
+++ meta.go
@@ -11,14 +11,34 @@ import codec "github.com/alacrity-engine/resource-codec"
 // SpritesheetMeta is spritesheet metadata
 // read from the YAML file.
 type SpritesheetMeta struct {
-	Name   string `yaml:"name"`
-	Width  int    `yaml:"width"`
-	Height int    `yaml:"height"`
+	Name   string   `yaml:"name"`
+	Width  int      `yaml:"width"`
+	Height int      `yaml:"height"`
+	Orig   OrigMeta `taml:"orig"`
+	Area   AreaMeta `yaml:"area"`
+}
+
+type OrigMeta struct {
+	X int `yaml:"x"`
+	Y int `yaml:"y"`
+}
+
+type AreaMeta struct {
+	PixelWidth  int `yaml:"pixelWidth"`
+	PixelHeight int `yaml:"pixelHeight"`
 }
 
 func (meta SpritesheetMeta) ToSpritesheetData() codec.SpritesheetData {
 	return codec.SpritesheetData{
 		Width:  int32(meta.Width),
 		Height: int32(meta.Height),
+		Orig: codec.OrigData{
+			X: int32(meta.Orig.X),
+			Y: int32(meta.Orig.Y),
+		},
+		Area: codec.AreaData{
+			PixelWidth:  int32(meta.Area.PixelWidth),
+			PixelHeight: int32(meta.Area.PixelHeight),
+		},
 	}
 }
